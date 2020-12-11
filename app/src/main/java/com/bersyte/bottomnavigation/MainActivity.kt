eb94e933c252db1c578b12a77161e1ae3106a07a
Isaias_Bernardo <bersyteinfo@gmail.com>
2020-12-12 01:35:08 +0200

    Starter Code

---
+++ app/src/main/java/com/bersyte/bottomnavigation/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.bersyte.bottomnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.bersyte.bottomnavigation.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding
+            .bottomNavigationView
+            .setupWithNavController(
+                navController
+            )
+
+        val appBarConfiguration =
+            AppBarConfiguration(
+                setOf(
+                R.id.dashboardFragment,
+                R.id.profileFragment,
+                R.id.settingFragment,
+            ))
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+}
